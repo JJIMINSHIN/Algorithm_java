c22c95f16fccc200dd8d03ed67ba2f72ee3ae3a0
JJIMINSHIN <scalla1031@gmail.com>
2023-06-04 14:53:09 +0900

    CREATE : ch01 기본 알고리즘 연습문제q1~q3

---
+++ algorithm/src/ch01/ch01_01.java
@@ -0,0 +1,37 @@
+package ch01;
+
+public class ch01_01 {
+
+    public static void main(String[] args) {
+    System.out.println("최대값은 :  "+max4(3,4,5,6));
+    System.out.println("최솟값은 :  "+min3(3,4,5));
+    System.out.println("최소값은 :  "+min4(4,5,6,2));
+
+
+    }
+
+    public static int max4(int a, int b ,int c, int d){
+        int max = a;
+        if(b> max) max = b;
+        if(c > max) max = c;
+        if(d> max) max = d;
+
+        return max;
+    }
+
+    public static int min3(int a, int b, int c){
+        int min =a;
+
+        if(min>b) min =b;
+        if(min>c) min =c;
+        return min;
+    }
+    public static int min4(int a, int b ,int c, int d){
+        int min=a;
+
+        if(min > b) min = b;
+        if(min> c) min = c;
+        if(min> d) min = d;
+        return min;
+    }
+}
