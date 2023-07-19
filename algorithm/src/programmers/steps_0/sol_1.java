f069a1687a981c9a7d981e4f5be8831d3387f40e
JJIMINSHIN <scalla1031@gmail.com>
2023-07-19 22:57:11 +0900

    CREATE : 접두사인지 확인하기

---
+++ algorithm/src/programmers/steps_0/sol_1.java
@@ -0,0 +1,21 @@
+package programmers.steps_0;
+
+public class sol_1 {
+    //접두사인지 확인하기
+
+    public static void main(String[] args) {
+        String my_string ="banana"; 
+        String is_prefix= "";
+        System.out.println(solution("",""));    
+    }
+
+    public static int solution(String my_string, String is_prefix) {
+        int answer = 0; 
+        int len = is_prefix.length();
+        if(my_string.length()>=len){
+            System.out.println(my_string.substring(0, len));
+            if(my_string.substring(0, len).equals(is_prefix)) answer =1;
+        }
+        return answer;
+    }
+}
