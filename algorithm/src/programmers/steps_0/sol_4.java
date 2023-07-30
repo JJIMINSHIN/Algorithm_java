963ff125deeccf365e23118e37579978fed01eee
JJIMINSHIN <scalla1031@gmail.com>
2023-07-30 14:48:02 +0900

    CREATE : 주사위 게임 1

---
+++ algorithm/src/programmers/steps_0/sol_4.java
@@ -0,0 +1,17 @@
+package programmers.steps_0;
+
+public class sol_4 {
+	//주사위 게임 1
+	
+	public static void main(String[] args) {
+		System.out.println(new sol_4().solution(3, 5));
+	};
+	
+	public int solution(int a, int b) {
+        int answer = 0;
+        if(a%2 !=0 && b%2 != 0) answer = (a*a) + (b*b);
+        else if(a%2 !=0 || b%2 !=0) answer = 2*(a+b);
+        else answer = Math.abs(a-b);
+        return answer;
+    }
+}
