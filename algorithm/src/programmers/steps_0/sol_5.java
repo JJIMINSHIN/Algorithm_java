757d1e254145645429b628772e1c0105c2d05a3c
JJIMINSHIN <scalla1031@gmail.com>
2023-07-30 15:52:03 +0900

    CREATE : 공백으로 구분하기 2

---
+++ algorithm/src/programmers/steps_0/sol_5.java
@@ -0,0 +1,28 @@
+package programmers.steps_0;
+
+import java.util.ArrayList;
+
+public class sol_5 {
+	// 공백으로 구분하기 2
+	
+	public static void main(String[] args) {
+		System.out.println(new sol_5().solution(" i    love  you"));
+	};
+	
+	public String[] solution(String my_string) {
+        String[] answer =  my_string.split(" ");
+        ArrayList<String> al = new ArrayList();
+        for(int i=0; i<answer.length; i++) {
+        	if(!answer[i].equals("")) al.add(answer[i]);
+        	
+        }
+        
+        answer = new String[al.size()];
+        for(int i = 0; i<al.size(); i++) {
+        	answer[i] = al.get(i);
+        }
+        
+        return answer;
+        
+    }
+}
