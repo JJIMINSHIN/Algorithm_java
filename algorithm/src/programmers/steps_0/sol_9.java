98db5e036e302d53074e5df8b9017d8f2f7872b2
JJIMINSHIN <86641936+JJIMINSHIN@users.noreply.github.com>
2023-08-06 22:26:15 +0900

    Create : A 강조하기

---
+++ algorithm/src/programmers/steps_0/sol_9.java
@@ -0,0 +1,10 @@
+
+// A 강조하기
+
+public class sol_9 {
+    public String solution(String myString) {
+        String answer = myString.toLowerCase();
+        return answer.replaceAll("a","A");
+    }
+
+}
