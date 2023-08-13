cd7ff879e3bc5d7a0f1da6b5d4b9acbd21b018cc
JJIMINSHIN <scalla1031@gmail.com>
2023-08-13 20:29:33 +0900

    CREATE: 문자열 돌리기

---
+++ algorithm/src/programmers/steps_0/sol_14.java
@@ -0,0 +1,18 @@
+package programmers.steps_0;
+
+import java.util.Scanner;
+
+
+//문자열 돌리기
+public class sol_14 {
+	
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for(int i=0; i<a.length(); i++) {
+        	System.out.println(a.charAt(i));
+        }
+        
+	};
+
+}
