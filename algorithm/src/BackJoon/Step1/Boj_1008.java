768902eea327f9cd0289c8580b67653b7dc7fcc2
지민 <scalla1031@gmail.com>
2024-05-01 19:43:27 +0900

    CREATE : A/B

--- algorithm/src/BackJoon/Step1/Boj_1008.java
+++ algorithm/src/BackJoon/Step1/Boj_1008.java
@@ -1,7 +1,7 @@
 package BackJoon.Step1;
 
 /*
- * 사칙연산 A×B
+ * 사칙연산 A/B
  * */
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,9 +10,13 @@ import java.util.StringTokenizer;
 
 public class Boj_1008 {
 	public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-    
-        System.out.println((Integer.parseInt(st.nextToken())) * Integer.parseInt(st.nextToken()));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        
+        double a = (double)Integer.parseInt(stringTokenizer.nextToken());
+        double b = (double)Integer.parseInt(stringTokenizer.nextToken());
+        
+        System.out.println(a / b);
+        bufferedReader.close();
     }
 }
