00e5c14103a8d4c37f86f84da0f0eb9a563fae08
지민 <scalla1031@gmail.com>
2024-05-01 19:44:50 +0900

    CREATE : 사칙연산

--- algorithm/src/BackJoon/Step1/Boj_10869.java
+++ algorithm/src/BackJoon/Step1/Boj_10869.java
@@ -1,5 +1,45 @@
 package BackJoon.Step1;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
+
 public class Boj_10869 {
+	
+	public static void Boj_10869(String args[]) throws IOException {
+		
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        
+        int a = Integer.parseInt(stringTokenizer.nextToken());
+        int b = Integer.parseInt(stringTokenizer.nextToken());
+        
+        System.out.println(new Boj_10869().getPlus(a,b));
+        System.out.println(new Boj_10869().getMinor(a,b));
+        System.out.println(new Boj_10869().getGob(a,b));
+        System.out.println(new Boj_10869().getMok(a,b));
+        System.out.println(new Boj_10869().getNa(a,b));
+        bufferedReader.close();
+    }
+    
+    public int getPlus(int a, int b) {
+        return a+b;
+    }
+    
+    public int getMinor(int a, int b) {
+        return a-b;
+    }
+    
+    public int getGob(int a, int b) {
+        return a*b;
+    }
+    
+    public int getMok(int a, int b) {
+        return a/b;
+    }
+    
+    public int getNa(int a, int b) {
+        return a%b;
+    }
 
 }
