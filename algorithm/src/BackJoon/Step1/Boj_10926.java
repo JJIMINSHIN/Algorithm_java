cf38449b4b66fb5475981d24153b1f1060d5e4bb
지민 <scalla1031@gmail.com>
2024-05-01 19:45:32 +0900

    CREATE : ??!

---
+++ algorithm/src/BackJoon/Step1/Boj_10926.java
@@ -0,0 +1,17 @@
+package BackJoon.Step1;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Boj_10926 {
+
+	 public static void main(String args[]) throws IOException {
+	        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+	        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+	        
+	        String res = stringTokenizer.nextToken();
+	        System.out.println(res.concat("??!"));
+
+	        bufferedReader.close();
+	    }
+}
