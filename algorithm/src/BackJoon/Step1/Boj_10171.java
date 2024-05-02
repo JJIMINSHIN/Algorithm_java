d242c1281a0cb7a70f22e693e7c80f96d65e8e4c
지민 <scalla1031@gmail.com>
2024-05-02 21:04:44 +0900

    CREATE : 고양이

---
+++ algorithm/src/BackJoon/Step1/Boj_10171.java
@@ -0,0 +1,15 @@
+package BackJoon.Step1;
+
+/*
+ * 메모리 	시간
+ * 11332KB	80ms
+ * */
+public class Boj_10171 {
+    public static void main(String[] agrs){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+
