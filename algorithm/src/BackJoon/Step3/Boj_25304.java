f83740cce6985720241c965007fa614788331b92
JJIMINSHIN <scalla1031@gmail.com>
2024-05-07 14:20:37 +0900

    CREATE : 영수증

---
+++ algorithm/src/BackJoon/Step3/Boj_25304.java
@@ -0,0 +1,33 @@
+package BackJoon.Step3;
+/*
+ * 문제 : 영수증
+ * Time : 80 ms
+ * */
+
+import java.io.*;
+import java.util.*;
+
+public class Boj_25304 {
+	public static void main(String args[]) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int total = Integer.parseInt(br.readLine());
+        int items = Integer.parseInt(br.readLine());
+        int sum =0;
+
+        for(int i=0; i<items; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());    
+            
+            int price = Integer.parseInt(st.nextToken());
+            int list =  Integer.parseInt(st.nextToken());
+            
+            sum += (price*list); 
+        }
+        
+        if(total == sum)
+            System.out.println("Yes");
+        else 
+            System.out.println("No");
+
+    }
+}
