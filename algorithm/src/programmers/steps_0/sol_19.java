155b84c50a5a908d3e1228e43a5cc85c01b3c1fe
JJIMINSHIN <scalla1031@gmail.com>
2024-05-26 16:26:21 +0900

    RENAME : Package Name programmers/steps_0 -> programmers

--- algorithm/src/programmers/steps_0/sol_19.java
+++
@@ -1,8 +0,0 @@
-package programmers.steps_0;
-
-//flag에 따라 다른 값 반환하기
-public class sol_19 {
-    public int solution(int a, int b, boolean flag) {
-        return flag == true ? a+b : a-b;
-    }
-}
--- algorithm/src/programmers/steps_0/sol_20.java
+++
@@ -1,8 +0,0 @@
-package programmers.steps_0;
-
-//문자열로 변환
-public class sol_20 {
-    public String solution(int n) {
-        return n+"";
-    }
-}
