94c71573675fb1136501d49c59595ba79ac0fbcf
지민 <scalla1031@gmail.com>
2024-06-21 21:49:41 +0900

    CREATE : 단어 길이 재기

---
+++ algorithm/src/BackJoon/Step5/Boj_2743.java
@@ -0,0 +1,16 @@
+package BackJoon.Step5;
+
+/*
+ * 문제 : 단어 길이 재기
+ * Time : 72 ms 
+ * */
+import java.io.*;
+
+public class Boj_2743 {
+	 public static void main(String args[]) throws IOException {
+	        
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        System.out.println(br.readLine().length());
+	    }
+
+}
