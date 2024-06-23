b00f36e9dccc23631420628335d3bf15597eff86
JJIMINSHIN <scalla1031@gmail.com>
2024-06-23 23:37:09 +0900

    CREATE : 아스키 코드

---
+++ algorithm/src/BackJoon/Step5/Boj_11654.java
@@ -0,0 +1,18 @@
+package BackJoon.Step5;
+
+/*
+ * 문제 : 아스키 코드
+ * Time : 64 ms 
+ * */
+import java.io.*;
+
+public class Boj_11654 {
+	public static void main(String args[]) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int asc = br.readLine().charAt(0);
+		System.out.println(asc);
+
+	}
+
+}
