0ef3f4e92e71ab6dba9d81e78f823a6879518e83
JJIMINSHIN <scalla1031@gmail.com>
2024-07-04 20:07:17 +0900

    CREATE : 4. 단어 뒤집기

---
+++ algorithm/src/inf/ch01/ch01_04.java
@@ -0,0 +1,25 @@
+package inf.ch01;
+
+import java.io.*;
+
+public class ch01_04 {
+	
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		StringBuilder sb = null;
+		String line = null;
+		for(int i=0; i<n; i++) {
+			line = br.readLine();
+			sb = new StringBuilder(line);
+			bw.write(sb.reverse().toString());
+			bw.newLine();
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
