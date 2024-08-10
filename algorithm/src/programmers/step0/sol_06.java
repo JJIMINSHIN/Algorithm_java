99a0fd9bbc12afce12b86d6c9aadb53a1decfb8c
JJIMINSHIN <scalla1031@gmail.com>
2024-08-10 17:18:48 +0900

    REFACT : 특정한 문자를 대문자로 바꾸기

--- algorithm/src/programmers/step0/sol_06.java
+++ algorithm/src/programmers/step0/sol_06.java
@@ -1,6 +1,14 @@
 package programmers.step0;
 
-// 특정한 문자를 대문자로 바꾸기
+/************
+ * @name : 특정한 문자를 대문자로 바꾸기
+ * @date : 2024/8/10 17:18 PM  
+ * @time :  0 (ms)
+ * @Description :
+ * 한문장이라 replaceAll -> replace로 변경
+ * 
+ ************/
+
 public class sol_06 {
 	
 	public static void main(String[] args) {
