1781bdce256e2bd66792a17b89fef7cbf0ba2b22
JJIMINSHIN <scalla1031@gmail.com>
2024-08-18 09:42:32 +0900

    REFAC : 공배수

--- algorithm/src/programmers/step0/sol_22.java
+++ algorithm/src/programmers/step0/sol_22.java
@@ -1,9 +1,21 @@
 package programmers.step0;
 
-//공배수
+/************
+ * @name : 공배수
+ * @date : 2024/5/26 15:43 PM
+ * @time :  0 (ms)
+ * @Description :
+ * 
+ ************/
 public class sol_22 {
 	public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
+		
 		System.out.println(new sol_22().solution(55, 10, 5));
+		
+		long endTime = System.currentTimeMillis();
+		long duTime = endTime - startTime;
+		System.out.println("active time = " + duTime + " m/s");
 	};
     public int solution(int number, int n, int m) {
         return number % n ==0 && number % m ==0 ? 1 : 0;
