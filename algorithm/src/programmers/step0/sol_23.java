f307179db974c52b865cf8d3c4bfa6793a130e4f
JJIMINSHIN <scalla1031@gmail.com>
2024-08-25 17:11:28 +0900

    Style : 접미사인지 확인하기

--- algorithm/src/programmers/step0/sol_23.java
+++ algorithm/src/programmers/step0/sol_23.java
@@ -1,6 +1,13 @@
 package programmers.step0;
 
-//접미사인지 확인하기
+/************
+ * @name : 접미사인지 확인하기
+ * @date : 2024/9/13 18:05 PM
+ * @time : 0 (ms)
+ * @Description : 
+ * 접두사인지 확인하려면 startWith 메서드 사용
+ * 접미사 확인하려면 endWith 메서드 사용
+ ************/
 public class sol_23 {
 	public static void main(String[] args) {
 		System.out.println(new sol_23().solution("banana", "wxyz"));
