aafb01e7615ad869fdd9f9549afabd4d9e8583f0
JJIMINSHIN <scalla1031@gmail.com>
2024-08-25 16:37:59 +0900

    Style : 주석 추가

--- algorithm/src/programmers/step0/sol_28.java
+++ algorithm/src/programmers/step0/sol_28.java
@@ -1,14 +1,16 @@
 package programmers.step0;
-/*
+/************
+ * @name : 순서 바꾸기
+ * @date : 2023/10/08 20:32 PM
+ * @time : 0 (ms)
+ * @Description : 
  *  System.arraycopy(Object src, int srcPos, Object dest, int destPos, int length)
  *  src : 복사하고자 하는 원본 배열
  *  srcPos : 원본 배열에서의 복사 시작할 인덱스
  *  dest : 복사한 원소를 저장할 대상 배열
  *  destPos : 대상 배열에서의 복사 대상 원소의 저장 시작 위치
  *  length : 복사할 원소의 개수 
- */
-
-//순서 바꾸기
+ ************/
 
 public class sol_28 {
 	public static void main(String[] args) {
