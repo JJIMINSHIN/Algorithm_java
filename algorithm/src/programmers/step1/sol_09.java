3d42150894519e9696b2854a9738357731f68439
JJIMINSHIN <scalla1031@gmail.com>
2024-09-10 10:38:10 +0900

    CHANGE : 주석 변경

--- algorithm/src/programmers/step1/sol_09.java
+++ algorithm/src/programmers/step1/sol_09.java
@@ -4,8 +4,7 @@ package programmers.step1;
  * @name : 짝수의 합
  * @date : 2024/09/08 17:58 PM
  * @time : 0 (ms)
- * @Description : 반올림 round 올림 ceil 내림 floor 까먹지 않기! (sol2, 다른사람 문제풀이 참고) 굳이
- *              double로 형변환하지 않고 분자값에 1000곱해서 진행하면 깔끔.
+ * @Description : 
  ************/
 
 public class sol_09 {
