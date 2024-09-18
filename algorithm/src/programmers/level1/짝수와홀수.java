f03cb47a7ae45032e0ca3ad52e6bdbacdad48537
JJIMINSHIN <scalla1031@gmail.com>
2024-09-18 17:30:35 +0900

    CREATE : 짝수와 홀수

---
+++ algorithm/src/programmers/level1/짝수와홀수.java
@@ -0,0 +1,15 @@
+package programmers.level1;
+
+/************
+ * @name : 짝수와홀수
+ * @date : 2024/09/18 16:30 PM
+ * @time : 0 (ms)
+ * @Description :
+ * 단순 구현 문제, 삼항 연산자로 해결
+ ************/
+
+public class 짝수와홀수 {
+	public String solution(int num) {
+		return num % 2 != 0 ? "Odd" : "Even";
+	}
+}
