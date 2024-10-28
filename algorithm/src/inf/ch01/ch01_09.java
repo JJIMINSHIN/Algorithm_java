dcb481192c0bb23a6bf65b351eda95665eb51b18
JJIMINSHIN <scalla1031@gmail.com>
2024-10-28 16:38:34 +0900

    CREATE : 가장 짧은 문자거리

---
+++ algorithm/src/inf/ch01/ch01_09.java
@@ -0,0 +1,28 @@
+package inf.ch01;
+
+import java.io.*;
+
+/************
+ * @name : 가장 짧은 문자거리
+ * @date : 2024/10/15 10:54 AM
+ * @time : 109 (ms)
+ * @Description :
+ * 8번을 활용해서 정규식으로 숫자 추출! 
+ * 정규식 표현법 계속 헷갈리는데 제발 헷갈리지 말자..
+ * ^=>문자열 시작 
+ * +$ : 문자열 종료
+ * [] 안에 써주어야함
+ ************/
+
+public class ch01_09 {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine().replaceAll("[^0-9+$]", "");
+		int num = Integer.parseInt(str);
+		System.out.println(num);
+	}
+	
+
+}
+
