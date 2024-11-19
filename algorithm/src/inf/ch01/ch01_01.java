dc2d9eebfe2cd30a358d1dbeae4406b831a1513c
JJIMINSHIN <scalla1031@gmail.com>
2024-11-19 17:19:26 +0900

    REFACT : 문자 찾기

--- algorithm/src/inf/ch01/ch01_01.java
+++ algorithm/src/inf/ch01/ch01_01.java
@@ -1,52 +1,62 @@
 package inf.ch01;
 
-public class ch01_01 {
+/************
+ * @name : 문자 찾기
+ * @date : 2024/01/20
+ * @time : 1 (ms)
+ * @Description :
+ ************/
 
-   //내코드
+
+public class ch01_01 {
+	
+	public static void main(String[] args) {
+		int an = new ch01_01().getCnt("Computercooler", "c");
+		System.out.println(an);
+	}
+
 	public int getCnt(String str, String c) {
-		int cnt =0;
+		int cnt = 0;
 		str = str.toLowerCase();
 		String[] result = str.split("");
-		for(String x: result) {
-			if(c.equals(x)) cnt++;
+		for (String x : result) {
+			if (c.equals(x))
+				cnt++;
 		}
 		return cnt;
 	}
-	
-	/* 
-	 * 강의에서의 최대값 최소값	  
-	 */
-	
-	 public static void main(String[] args) {
-		    System.out.println("최대값은 :  "+max4(3,4,5,6));
-		    System.out.println("최솟값은 :  "+min3(3,4,5));
-		    System.out.println("최소값은 :  "+min4(4,5,6,2));
-		    }
+//*************** 강의 ******************///
+	public int max4(int a, int b, int c, int d) {
+		int max = a;
+		if (b > max)
+			max = b;
+		if (c > max)
+			max = c;
+		if (d > max)
+			max = d;
 
-		    public static int max4(int a, int b ,int c, int d){
-		        int max = a;
-		        if(b> max) max = b;
-		        if(c > max) max = c;
-		        if(d> max) max = d;
+		return max;
+	}
 
-		        return max;
-		    }
+	public int min3(int a, int b, int c) {
+		int min = a;
 
-		    public static int min3(int a, int b, int c){
-		        int min =a;
+		if (min > b)
+			min = b;
+		if (min > c)
+			min = c;
+		return min;
+	}
 
-		        if(min>b) min =b;
-		        if(min>c) min =c;
-		        return min;
-		    }
-		    public static int min4(int a, int b ,int c, int d){
-		        int min=a;
+	public int min4(int a, int b, int c, int d) {
+		int min = a;
 
-		        if(min > b) min = b;
-		        if(min> c) min = c;
-		        if(min> d) min = d;
-		        return min;
-		    }
-		    
-		    
+		if (min > b)
+			min = b;
+		if (min > c)
+			min = c;
+		if (min > d)
+			min = d;
+		return min;
+	}
 }
