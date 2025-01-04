d1562ddb2557a8fb598cedaf8c79ff8b6d99a596
JJIMINSHIN <scalla1031@gmail.com>
2025-01-04 22:57:47 +0900

    CREATE : 정수 내림차순으로 배치하기

---
+++ algorithm/src/programmers/level1/정수_내림차순으로_배치하기.java
@@ -0,0 +1,24 @@
+package programmers.level1;
+
+import java.util.*;
+
+/************
+ * @name : 정수 내림차순으로 배치하기
+ * @date : 2025/01/03 17:02 PM
+ * @time : 45 (ms)
+ * @Description :
+ * 구글 검색해보니 string 배열 내림차순할때 
+ * Arrays.sort() 와 Collections.reverseOrder를 조합해서 쓰길래 적용해봄
+ ************/
+
+public class 정수_내림차순으로_배치하기 {
+	public long solution(long n) {
+        String answer = "";
+        String[] str = (n+"").split("");
+        Arrays.sort(str, Collections.reverseOrder());
+        for(String s : str) {
+        	answer+= s;
+        }
+        return Long.parseLong(answer);
+    }
+}
