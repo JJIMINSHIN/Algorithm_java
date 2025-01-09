c4ddb63fb3c51d978b050a92a0eeb3f01d343eb7
JJIMINSHIN <scalla1031@gmail.com>
2025-01-09 15:28:05 +0900

    CREATE : 진법 변환

---
+++ algorithm/src/BackJoon/Step8/Boj_2745.java
@@ -0,0 +1,33 @@
+package BackJoon.Step8;
+
+import java.io.*;
+import java.util.*;
+
+/************
+ * @info : 세로읽기
+ * @date : 2025/01/07 15:25 PM
+ * @time : 84(ms)
+ * @Description :
+ * 진법 변환
+ * N진법 -> 10진법
+ * int n = Intger.parseInt(String, 해당(3진법이였으면 3);
+ * 10진법 - > n진법
+ * String str = Integer.toString(10진수 int, 바꿀진법);
+ ************/
+
+public class Boj_2745 {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		String n = st.nextToken();
+		int b = Integer.parseInt(st.nextToken());
+				
+		int bz = Integer.parseInt(n,b);
+		
+		System.out.println(bz);
+		
+		br.close();
+	}
+	
+
+}
