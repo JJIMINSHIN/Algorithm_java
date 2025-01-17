c56b7d29c9a858fcbe49fe107af16d8484b0077f
JJIMINSHIN <scalla1031@gmail.com>
2025-01-17 16:19:08 +0900

    RENAME : 짝수와 홀수

--- algorithm/src/programmers/level1/_20240918.java
+++ algorithm/src/programmers/level1/_20240918.java
@@ -1,14 +1,14 @@
 package programmers.level1;
 
 /************
- * @name : 짝수와홀수
+ * @name : 짝수와 홀수
  * @date : 2024/09/18 16:30 PM
  * @time : 0 (ms)
  * @Description :
  * 단순 구현 문제, 삼항 연산자로 해결
  ************/
 
-public class pro_짝수와홀수 {
+public class _20240918 {
 	public String solution(int num) {
 		return num % 2 != 0 ? "Odd" : "Even";
 	}
