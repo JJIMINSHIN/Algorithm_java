8a176043b6851b66f2658a37b869a622bb153a70
JJIMINSHIN <scalla1031@gmail.com>
2025-01-19 22:21:42 +0900

    CREATE : 문자열을 정수로 바꾸기

---
+++ algorithm/src/programmers/level1/_20250114_01.java
@@ -0,0 +1,18 @@
+package programmers.level1;
+
+/************
+ * @name : 문자열을 정수로 바꾸기
+ * @date : 2025/01/14 16:38 PM
+ * @time : 0 (ms)
+ * @Description :
+ * 단순 구현 문제
+ ************/
+
+
+public class _20250114_01 {
+	
+    public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+
+}
